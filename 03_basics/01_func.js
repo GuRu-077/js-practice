7bd71467ebd8260be2437d7a8a5a71cd764c6c6b
GuRu <guruvendra81@gmail.com>
2026-01-09 08:19:27 +0000

    functions

---
+++ 03_basics/01_func.js
@@ -0,0 +1,16 @@
+ function addTwoNumbers(num1,num2){
+    // let result = num1 + num2
+    // return result
+    return num1 + num2
+ }
+ 
+ const result = addTwoNumbers(3,5)
+
+ addTwoNumbers(3,5)
+
+//  console.log("Result :", result);
+
+function loginUserMessage(username){
+    return `${username} just logged in`  //String Interpolation
+}
+console.log(loginUserMessage("Guru"));
