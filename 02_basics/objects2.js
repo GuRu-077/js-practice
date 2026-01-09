8ea1a1b5f0b71ed1b7e81ff1b2a6681d8bf76cc3
GuRu <guruvendra81@gmail.com>
2026-01-09 07:51:32 +0000

    Objects Completed

---
+++ 02_basics/objects2.js
@@ -0,0 +1,12 @@
+const course = {
+    coursename: "Js crash course",
+    price: "999",
+    courseInstructor: "Guru"
+}
+
+const {courseInstructor : Instructor} = course // Extracts value from object. and we can also assign new name fro easy use.
+//console.log(courseInstructor); 
+console.log(Instructor); 
+
+
+
