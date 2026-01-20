e83622fd992192251a036dc49513fc6d910cc92c
GuRu <guruvendra81@gmail.com>
2026-01-20 08:03:05 +0000

    modified:   03_basics/01_func.js

--- 03_basics/01_func.js
+++ 03_basics/01_func.js
@@ -11,6 +11,24 @@
 //  console.log("Result :", result);
 
 function loginUserMessage(username){
-    return `${username} just logged in`  //String Interpolation
+    if(username === undefined){
+        // console.log("Please Enter a Valid User Name!");
+        return
+    }
+    return `${username} just logged in`  //String Interpolation--
 }
-console.log(loginUserMessage("Guru"));
+// console.log(loginUserMessage("Guru")); // Agar yaha kuch value pass nhi karenge to undefined aayega--
+
+function CalculateCartPrice(val1 , val2 ,...num1){   //yaha val1 aur val 2 me 200,400 jayega
+    return num1
+}
+// console.log(CalculateCartPrice(200,400,500));
+
+const user ={
+    username : "Guru Chaudhary",
+    age : 20
+}
+function handleObjects(anyObject){
+    console.log(`Username is ${anyObject.username} and Age is ${anyObject.age}`);
+}
+handleObjects(user)
