8bc2ab856c5fec143d6859b95e94cafb1337f876
GuRu <guruvendra81@gmail.com>
2026-01-20 08:03:18 +0000

    new file:   04_ControlFlow/thruthy.js

---
+++ 04_ControlFlow/thruthy.js
@@ -0,0 +1,14 @@
+const userEmail = "Guru@ai"
+
+if(userEmail){
+    console.log("Got User Email");    
+}
+else{
+    console.log("Dont Have user Email");
+}
+
+// Falsy Values
+// false, 0 , -0 , BigInt, undefined , NAN , null , ""
+
+// Truthy Values
+// "0","false"," ",[],{}, function(){}
